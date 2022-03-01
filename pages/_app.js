c2f03bca9c12f935ff17201e60b4b416ee44ad62
Azarov Serge <s.a.azarov@gmail.com>
2022-03-02 01:55:01 +0300

    ui | Refactoring app

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import Head from 'next/head';
-import Layout from '../components/layout/layout.js';
-import { ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { useRouter } from 'next/router';
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import Head from "next/head";
+import Layout from "../components/layout/layout.js";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { useRouter } from "next/router";
 
-import lightTheme from '../theme/light';
-import darkTheme from '../theme/dark';
+import { AppThemeProvider, useAppTheme } from "../ui/AppThemeProvider";
+import darkTheme from "../theme/dark";
 
-import Configure from './configure';
+import Configure from "./configure";
 
-import stores from '../stores/index.js';
+import stores from "../stores/index.js";
 
-import { ACTIONS } from '../stores/constants';
-import '../styles/global.css'
+import { ACTIONS } from "../stores/constants";
+import "../styles/global.css";
+import "../styles/variables.css";
 
 export default function MyApp({ Component, pageProps }) {
   const router = useRouter();
+  const { appTheme, setAppTheme } = useAppTheme();
 
-  const [themeConfig, setThemeConfig] = useState(darkTheme);
   const [stalbeSwapConfigured, setStableSwapConfigured] = useState(false);
   const [accountConfigured, setAccountConfigured] = useState(false);
 
   useEffect(() => {
     // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
+    const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles);
     }
@@ -45,8 +46,10 @@ export default function MyApp({ Component, pageProps }) {
   };
 
   useEffect(function () {
-    const localStorageDarkMode = window.localStorage.getItem('yearn.finance-dark-mode');
-    changeTheme(localStorageDarkMode ? localStorageDarkMode === 'dark' : false);
+    const localStorageDarkMode = window.localStorage.getItem(
+      "yearn.finance-dark-mode"
+    );
+    changeTheme(localStorageDarkMode ? localStorageDarkMode === "dark" : false);
   }, []);
 
   useEffect(function () {
@@ -56,14 +59,20 @@ export default function MyApp({ Component, pageProps }) {
     stores.dispatcher.dispatch({ type: ACTIONS.CONFIGURE });
 
     return () => {
-      stores.emitter.removeListener(ACTIONS.CONFIGURED_SS, stalbeSwapConfigureReturned);
-      stores.emitter.removeListener(ACTIONS.ACCOUNT_CONFIGURED, accountConfigureReturned);
+      stores.emitter.removeListener(
+        ACTIONS.CONFIGURED_SS,
+        stalbeSwapConfigureReturned
+      );
+      stores.emitter.removeListener(
+        ACTIONS.ACCOUNT_CONFIGURED,
+        accountConfigureReturned
+      );
     };
   }, []);
 
   const validateConfigured = () => {
     switch (router.pathname) {
-      case '/':
+      case "/":
         return accountConfigured;
       default:
         return accountConfigured;
@@ -74,18 +83,21 @@ export default function MyApp({ Component, pageProps }) {
     <React.Fragment>
       <Head>
         <title>Solidly</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
       </Head>
-      <ThemeProvider theme={themeConfig}>
+      <AppThemeProvider value={{ appTheme, setAppTheme }}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
-        { validateConfigured() && (
-          <Layout changeTheme={changeTheme}>
+        {validateConfigured() && (
+          <Layout>
             <Component {...pageProps} changeTheme={changeTheme} />
           </Layout>
         )}
         {!validateConfigured() && <Configure {...pageProps} />}
-      </ThemeProvider>
+      </AppThemeProvider>
     </React.Fragment>
   );
 }
