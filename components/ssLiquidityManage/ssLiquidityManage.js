f979d0bef4e67b0bc68cdad83e0bdfc0b471a29c
Maksim Goncharov <magoncharov@tsum.com>
2022-05-03 20:21:03 +0300

    fix 3.16

--- components/ssLiquidityManage/ssLiquidityManage.js
+++ components/ssLiquidityManage/ssLiquidityManage.js
@@ -28,8 +28,6 @@ import {
   Search,
   DeleteOutline,
   ArrowBackIosNew,
-  RadioButtonUnchecked,
-  RadioButtonChecked,
   Close, Settings,
 } from '@mui/icons-material';
 import { useAppThemeContext } from '../../ui/AppThemeProvider';
@@ -1129,21 +1127,24 @@ export default function ssLiquidityManage() {
           }}>
 
           {!stable &&
-            <RadioButtonUnchecked
-              style={{
-                width: 20,
-                height: 20,
-                color: '#0B5E8E',
-              }}/>
+            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M0.5 10C0.5 4.7533 4.7533 0.5 10 0.5C15.2467 0.5 19.5 4.7533 19.5 10C19.5 15.2467 15.2467 19.5 10 19.5C4.7533 19.5 0.5 15.2467 0.5 10Z"
+                fill={appTheme === 'dark' ? '#151718' : '#DBE6EC'}
+                stroke={appTheme === 'dark' ? '#4CADE6' : '#0B5E8E'}/>
+            </svg>
           }
 
           {stable &&
-            <RadioButtonChecked
-              style={{
-                width: 20,
-                height: 20,
-                color: '#0B5E8E',
-              }}/>
+            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M0.5 10C0.5 4.7533 4.7533 0.5 10 0.5C15.2467 0.5 19.5 4.7533 19.5 10C19.5 15.2467 15.2467 19.5 10 19.5C4.7533 19.5 0.5 15.2467 0.5 10Z"
+                fill={appTheme === 'dark' ? '#151718' : '#DBE6EC'}
+                stroke={appTheme === 'dark' ? '#4CADE6' : '#0B5E8E'}/>
+              <path
+                d="M5 10C5 7.23858 7.23858 5 10 5C12.7614 5 15 7.23858 15 10C15 12.7614 12.7614 15 10 15C7.23858 15 5 12.7614 5 10Z"
+                fill={appTheme === 'dark' ? '#4CADE6' : '#0B5E8E'}/>
+            </svg>
           }
 
           <Typography
@@ -1159,21 +1160,24 @@ export default function ssLiquidityManage() {
           }}>
 
           {stable &&
-            <RadioButtonUnchecked
-              style={{
-                width: 20,
-                height: 20,
-                color: '#0B5E8E',
-              }}/>
+            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M0.5 10C0.5 4.7533 4.7533 0.5 10 0.5C15.2467 0.5 19.5 4.7533 19.5 10C19.5 15.2467 15.2467 19.5 10 19.5C4.7533 19.5 0.5 15.2467 0.5 10Z"
+                fill={appTheme === 'dark' ? '#151718' : '#DBE6EC'}
+                stroke={appTheme === 'dark' ? '#4CADE6' : '#0B5E8E'}/>
+            </svg>
           }
 
           {!stable &&
-            <RadioButtonChecked
-              style={{
-                width: 20,
-                height: 20,
-                color: '#0B5E8E',
-              }}/>
+            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M0.5 10C0.5 4.7533 4.7533 0.5 10 0.5C15.2467 0.5 19.5 4.7533 19.5 10C19.5 15.2467 15.2467 19.5 10 19.5C4.7533 19.5 0.5 15.2467 0.5 10Z"
+                fill={appTheme === 'dark' ? '#151718' : '#DBE6EC'}
+                stroke={appTheme === 'dark' ? '#4CADE6' : '#0B5E8E'}/>
+              <path
+                d="M5 10C5 7.23858 7.23858 5 10 5C12.7614 5 15 7.23858 15 10C15 12.7614 12.7614 15 10 15C7.23858 15 5 12.7614 5 10Z"
+                fill={appTheme === 'dark' ? '#4CADE6' : '#0B5E8E'}/>
+            </svg>
           }
 
           <Typography
