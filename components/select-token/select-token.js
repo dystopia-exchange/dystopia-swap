ad642ecd5e32a8730e8c5c6f00665583a4b3be62
Maksim Goncharov <magoncharov@tsum.com>
2022-05-05 01:41:31 +0300

    whitelist page

--- components/select-token/select-token.js
+++ components/select-token/select-token.js
@@ -3,13 +3,35 @@ import React from 'react';
 import { useAppThemeContext } from '../../ui/AppThemeProvider';
 import classes from './select-token.module.css';
 import { formatCurrency } from '../../utils';
-import { ArrowDropDownCircleOutlined } from '@mui/icons-material';
 
 function TokenSelect(props) {
   const {appTheme} = useAppThemeContext();
   const {value, options, symbol, handleChange, placeholder = ''} = props;
 
-  const noValue = value === null
+  const noValue = value === null;
+
+  const arrowIcon = () => {
+    return (
+      <svg
+        style={{marginRight: 10}}
+        width="32px" height="32px" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M1,16 C1,7.7157 7.716,1 16,1 L16,1 C24.284,1 31,7.7157 31,16 L31,16 C31,24.2843 24.284,31 16,31 L16,31 C7.716,31 1,24.2843 1,16 L1,16 Z"
+          fill={appTheme === 'dark' ? '#24292D' : '#CFE5F2'}>
+        </path>
+
+        <polygon
+          fill={appTheme === 'dark' ? '#5F7285' : '#86B9D6'}
+          points="16 16.9768 20.125 12.8518 21.303 14.0302 16 19.3335 10.697 14.0302 11.875 12.8518">
+        </polygon>
+
+        <path
+          d="M16,30 C8.268,30 2,23.732 2,16 L0,16 C0,24.8366 7.163,32 16,32 L16,30 Z M30,16 C30,23.732 23.732,30 16,30 L16,32 C24.837,32 32,24.8366 32,16 L30,16 Z M16,2 C23.732,2 30,8.268 30,16 L32,16 C32,7.1634 24.837,0 16,0 L16,2 Z M16,0 C7.163,0 0,7.1634 0,16 L2,16 C2,8.268 8.268,2 16,2 L16,0 Z"
+          fill={appTheme === 'dark' ? '#5F7285' : '#86B9D6'}>
+        </path>
+      </svg>
+    );
+  };
 
   return (
     <Select
@@ -26,14 +48,22 @@ function TokenSelect(props) {
         renderValue: noValue ? (selected) => {
           if (selected === null) {
             return (
-              <em style={{ padding: 10, paddingRight: 30 }}>
+              <div
+                style={{
+                  padding: 10,
+                  paddingRight: 30,
+                  fontWeight: 500,
+                  fontSize: 18,
+                  color: appTheme === 'dark' ? '#5F7285' : '#0B5E8E',
+                }}>
                 {placeholder}
-              </em>
-            )}
-        } : undefined
+              </div>
+            );
+          }
+        } : undefined,
       }}
       onChange={handleChange}
-      IconComponent={ArrowDropDownCircleOutlined}
+      IconComponent={arrowIcon}
       inputProps={{
         className: appTheme === 'dark' ? classes['tokenSelectInput--dark'] : classes.tokenSelectInput,
       }}>
--- components/ssWhitelist/ssWhitelist.js
+++ components/ssWhitelist/ssWhitelist.js
@@ -1,237 +1,300 @@
-import React, { useState, useEffect, useCallback } from 'react'
-import { Paper, TextField, InputAdornment, CircularProgress, Typography, Tooltip, Button, Select, MenuItem, Grid } from '@mui/material'
-import BigNumber from 'bignumber.js'
-
-import classes from './ssWhitelist.module.css'
-import { Search } from '@mui/icons-material'
-
-import stores from '../../stores'
-import { ACTIONS, ETHERSCAN_URL } from '../../stores/constants'
-import { formatAddress, formatCurrency } from '../../utils'
+import React, { useState, useEffect, useCallback } from 'react';
+import {
+  Paper,
+  TextField,
+  InputAdornment,
+  CircularProgress,
+  Typography,
+  Tooltip,
+  Button,
+  Select,
+  MenuItem,
+  Grid,
+} from '@mui/material';
+import BigNumber from 'bignumber.js';
+import TokenSelect from '../select-token/select-token';
+import classes from './ssWhitelist.module.css';
+import stores from '../../stores';
+import { ACTIONS, ETHERSCAN_URL } from '../../stores/constants';
+import { formatAddress, formatCurrency } from '../../utils';
 import { formatSymbol } from '../../utils';
+import { useAppThemeContext } from '../../ui/AppThemeProvider';
 
 export default function ssWhitelist() {
 
-  const [ web3, setWeb3 ] = useState(null)
-  const [ loading, setLoading ] = useState(false)
-  const [ whitelistLoading, setWhitelistLoading ] = useState(false)
-  const [ search, setSearch ] = useState('')
-  const [ token, setToken ] = useState(null)
-  const [ nfts, setNFTS ] = useState([])
-  const [ nft, setNFT ] = useState(null)
-  const [ veToken, setVeToken ] = useState(null)
+  const [web3, setWeb3] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [whitelistLoading, setWhitelistLoading] = useState(false);
+  const [search, setSearch] = useState('');
+  const [token, setToken] = useState(null);
+  const [nfts, setNFTS] = useState([]);
+  const [nft, setNFT] = useState(null);
+  const [veToken, setVeToken] = useState(null);
+  const {appTheme} = useAppThemeContext();
 
   const onSearchChanged = (event) => {
-    setSearch(event.target.value)
-    if(web3 && web3.utils.isAddress(event.target.value)) {
-      setLoading(true)
-      stores.dispatcher.dispatch({ type: ACTIONS.SEARCH_WHITELIST, content: { search: event.target.value } })
+    setSearch(event.target.value);
+    if (web3 && web3.utils.isAddress(event.target.value)) {
+      setLoading(true);
+      stores.dispatcher.dispatch({type: ACTIONS.SEARCH_WHITELIST, content: {search: event.target.value}});
     }
-  }
+  };
 
   useEffect(() => {
     const searchReturned = async (res) => {
-      setToken(res)
-      setLoading(false)
-    }
+      setToken(res);
+      setLoading(false);
+    };
 
     const whitelistReturned = async (res) => {
-      setWhitelistLoading(false)
-    }
+      setWhitelistLoading(false);
+    };
 
     const ssUpdated = () => {
-      setVeToken(stores.stableSwapStore.getStore('veToken'))
-      const nfts = stores.stableSwapStore.getStore('vestNFTs')
-      setNFTS(nfts)
+      setVeToken(stores.stableSwapStore.getStore('veToken'));
 
-      if(nfts && nfts.length > 0) {
-        setNFT(nfts[0])
+      const nfts = stores.stableSwapStore.getStore('vestNFTs');
+      /*const nfts = [
+        {
+          "id": "3",
+          "lockEnds": "1776297600",
+          "lockAmount": "1331994.000000000000000000",
+          "lockValue": "1316652.680121337436847399",
+        },
+        {
+          "id": "4",
+          "lockEnds": "1776297600",
+          "lockAmount": "1118072.000000000000000000",
+          "lockValue": "1105194.539441336798729261",
+        },
+        {
+          "id": "15",
+          "lockEnds": "1776297600",
+          "lockAmount": "676304.000000000000000000",
+          "lockValue": "668514.628576991346531549",
+        },
+        {
+          "id": "6",
+          "lockEnds": "1776297600",
+          "lockAmount": "1023840.000000000000000000",
+          "lockValue": "1012047.862089041025858407",
+        },
+        {
+          "id": "14",
+          "lockEnds": "1776297600",
+          "lockAmount": "677507.000000000000000000",
+          "lockValue": "669703.772953156656524642",
+        },
+        {
+          "id": "13",
+          "lockEnds": "1776297600",
+          "lockAmount": "681101.000000000000000000",
+          "lockValue": "673256.378845042060782790",
+        },
+        {
+          "id": "9",
+          "lockEnds": "1776297600",
+          "lockAmount": "795726.000000000000000000",
+          "lockValue": "786561.178610587794780056",
+        },
+        {
+          "id": "10",
+          "lockEnds": "1776297600",
+          "lockAmount": "763362.000000000000000000",
+          "lockValue": "754569.932899686001888625",
+        },
+        {
+          "id": "11",
+          "lockEnds": "1776297600",
+          "lockAmount": "727329.000000000000000000",
+          "lockValue": "718951.945113846051442262",
+        },
+        {
+          "id": "12",
+          "lockEnds": "1776297600",
+          "lockAmount": "688233.000000000000000000",
+          "lockValue": "680306.235612133731215627",
+        },
+      ];*/
+
+      setNFTS(nfts);
+
+      if (nfts && nfts.length > 0) {
+        setNFT(nfts[0]);
       }
-    }
+    };
 
     const accountChanged = async () => {
-      const w3 = await stores.accountStore.getWeb3Provider()
-      setWeb3(w3)
-    }
+      const w3 = await stores.accountStore.getWeb3Provider();
+      setWeb3(w3);
+    };
 
     const errorReturned = () => {
-      setWhitelistLoading(false)
-    }
+      setWhitelistLoading(false);
+    };
 
-    stores.emitter.on(ACTIONS.ERROR, errorReturned)
-    stores.emitter.on(ACTIONS.UPDATED, ssUpdated)
-    stores.emitter.on(ACTIONS.ACCOUNT_CHANGED, accountChanged)
-    stores.emitter.on(ACTIONS.ACCOUNT_CONFIGURED, accountChanged)
-    stores.emitter.on(ACTIONS.SEARCH_WHITELIST_RETURNED, searchReturned)
-    stores.emitter.on(ACTIONS.WHITELIST_TOKEN_RETURNED, whitelistReturned)
+    stores.emitter.on(ACTIONS.ERROR, errorReturned);
+    stores.emitter.on(ACTIONS.UPDATED, ssUpdated);
+    stores.emitter.on(ACTIONS.ACCOUNT_CHANGED, accountChanged);
+    stores.emitter.on(ACTIONS.ACCOUNT_CONFIGURED, accountChanged);
+    stores.emitter.on(ACTIONS.SEARCH_WHITELIST_RETURNED, searchReturned);
+    stores.emitter.on(ACTIONS.WHITELIST_TOKEN_RETURNED, whitelistReturned);
 
-    accountChanged()
+    accountChanged();
 
     return () => {
-      stores.emitter.removeListener(ACTIONS.ERROR, errorReturned)
-      stores.emitter.removeListener(ACTIONS.UPDATED, ssUpdated)
-      stores.emitter.removeListener(ACTIONS.ACCOUNT_CHANGED, accountChanged)
-      stores.emitter.removeListener(ACTIONS.ACCOUNT_CONFIGURED, accountChanged)
-      stores.emitter.removeListener(ACTIONS.SEARCH_WHITELIST_RETURNED, searchReturned)
-      stores.emitter.removeListener(ACTIONS.WHITELIST_TOKEN_RETURNED, whitelistReturned)
-    }
-  }, [])
+      stores.emitter.removeListener(ACTIONS.ERROR, errorReturned);
+      stores.emitter.removeListener(ACTIONS.UPDATED, ssUpdated);
+      stores.emitter.removeListener(ACTIONS.ACCOUNT_CHANGED, accountChanged);
+      stores.emitter.removeListener(ACTIONS.ACCOUNT_CONFIGURED, accountChanged);
+      stores.emitter.removeListener(ACTIONS.SEARCH_WHITELIST_RETURNED, searchReturned);
+      stores.emitter.removeListener(ACTIONS.WHITELIST_TOKEN_RETURNED, whitelistReturned);
+    };
+  }, []);
 
   const onAddressClick = (address) => {
-    window.open(`${ETHERSCAN_URL}token/${address}`, '_blank')
-  }
+    window.open(`${ETHERSCAN_URL}token/${address}`, '_blank');
+  };
 
   const onWhitelist = () => {
-    setWhitelistLoading(true)
-    stores.dispatcher.dispatch({ type: ACTIONS.WHITELIST_TOKEN, content: { token, nft } })
-  }
+    setWhitelistLoading(true);
+    stores.dispatcher.dispatch({type: ACTIONS.WHITELIST_TOKEN, content: {token, nft}});
+  };
 
   const handleChange = (event) => {
-    setNFT(event.target.value)
-  }
+    setNFT(event.target.value);
+  };
 
   const renderToken = () => {
     return (
-      
-      <Paper className={ classes.tokenContainer }>
-        <div className={ classes.inline }>
-          <img src={token.logoURI} alt='' width='70' height='70' className={ classes.tokenLogo } />
+
+      <Paper className={classes.tokenContainer}>
+        <div className={classes.inline}>
+          <img src={token.logoURI} alt="" width="70" height="70" className={classes.tokenLogo}/>
           <div>
-            <Typography className={ classes.tokenName } variant='h2'>{token.name}</Typography>
-            <Tooltip title='View in explorer'>
-              <Typography className={ classes.tokenAddress } color='textSecondary' onClick={ () => { onAddressClick(token.address) } }>{formatAddress(token.address)}</Typography>
+            <Typography className={classes.tokenName} variant="h2">{token.name}</Typography>
+            <Tooltip title="View in explorer">
+              <Typography className={classes.tokenAddress} color="textSecondary" onClick={() => {
+                onAddressClick(token.address);
+              }}>{formatAddress(token.address)}</Typography>
             </Tooltip>
           </div>
         </div>
-        <div className={ classes.whitelistStatus }>
-          <div className={ classes.whitelistContainer }>
+        <div className={classes.whitelistStatus}>
+          <div className={classes.whitelistContainer}>
             <div>
-              <Typography className={ classes.listingFee} color='textSecondary'>Whitelist Status</Typography>
-              { token.isWhitelisted &&
-                <Typography className={ classes.isWhitelist}>{ 'Whitelisted' }</Typography>
+              <Typography className={classes.listingFee} color="textSecondary">Whitelist Status</Typography>
+              {token.isWhitelisted &&
+                <Typography className={classes.isWhitelist}>{'Whitelisted'}</Typography>
               }
-              { !token.isWhitelisted &&
-                <Typography className={ classes.notWhitelist}>{ 'Not Whitelisted' }</Typography>
+              {!token.isWhitelisted &&
+                <Typography className={classes.notWhitelist}>{'Not Whitelisted'}</Typography>
               }
             </div>
             {
               !token.isWhitelisted &&
-              <Tooltip title='Listing fee either needs to be locked in your veToken NFT or be paid and burnt on list'>
+              <Tooltip title="Listing fee either needs to be locked in your veToken NFT or be paid and burnt on list">
                 <div>
-                  <Typography className={ classes.listingFee} color='textSecondary'>Listing Fee</Typography>
-                  <Typography className={ classes.listingFee}>{formatCurrency(token.listingFee)} {formatSymbol(veToken?.symbol)}</Typography>
+                  <Typography className={classes.listingFee} color="textSecondary">Listing Fee</Typography>
+                  <Typography
+                    className={classes.listingFee}>{formatCurrency(token.listingFee)} {formatSymbol(veToken?.symbol)}</Typography>
                 </div>
               </Tooltip>
             }
           </div>
           <div>
-            { !token.isWhitelisted && nft && BigNumber(nft.lockValue).gt(token.listingFee) &&
+            {!token.isWhitelisted && nft && BigNumber(nft.lockValue).gt(token.listingFee) &&
               <Button
-                variant='contained'
-                size='large'
-                color='primary'
-                onClick={ onWhitelist }
-                className={ classes.buttonOverride }
-                disabled={ whitelistLoading }
+                variant="contained"
+                size="large"
+                color="primary"
+                onClick={onWhitelist}
+                className={classes.buttonOverride}
+                disabled={whitelistLoading}
               >
-                <Typography className={ classes.actionButtonText }>{ whitelistLoading ? `Whitelisting` : `Whitelist` }</Typography>
-                { whitelistLoading && <CircularProgress size={10} className={ classes.loadingCircle } /> }
+                <Typography
+                  className={classes.actionButtonText}>{whitelistLoading ? `Whitelisting` : `Whitelist`}</Typography>
+                {whitelistLoading && <CircularProgress size={10} className={classes.loadingCircle}/>}
               </Button>
             }
-            { !token.isWhitelisted && (!nft || BigNumber(nft.lockValue).lt(token.listingFee)) &&
+            {!token.isWhitelisted && (!nft || BigNumber(nft.lockValue).lt(token.listingFee)) &&
               <Button
-                variant='contained'
-                size='large'
-                color='primary'
-                className={ classes.buttonOverride }
-                disabled={ true }
+                variant="contained"
+                size="large"
+                color="primary"
+                className={classes.buttonOverride}
+                disabled={true}
               >
-                <Typography className={ classes.actionButtonText }>{`Vest value < Fee`}</Typography>
+                <Typography className={classes.actionButtonText}>{`Vest value < Fee`}</Typography>
               </Button>
             }
           </div>
         </div>
       </Paper>
-    )
-  }
-
-  const renderMediumInput = (value, options) => {
-    return (
-      <div className={ classes.textField}>
-        <div className={ classes.mediumInputContainer}>
-          <Grid container>
-            <Grid item lg='auto' md='auto' sm={12} xs={12}>
-              <Typography variant="body2" className={ classes.smallText }>Please select your veNFT:</Typography>
-            </Grid>
-
-            <Grid item lg={6} md={6} sm={12} xs={12}>
-              <div className={ classes.mediumInputAmount }>
-                <Select
-                  fullWidth
-                  value={ value }
-                  onChange={handleChange}
-                  inputProps={{
-                    className: classes.mediumInput,
-                  }}
-                >
-                  { options && options.map((option) => {
-                    return (
-                      <MenuItem key={option.id} value={option}>
-                        <div className={ classes.menuOption }>
-                          <Typography>Token #{option.id}</Typography>
-                          <div>
-                            <Typography align='right' className={ classes.smallerText }>{ formatCurrency(option.lockValue) }</Typography>
-                            <Typography color='textSecondary' className={ classes.smallerText }>{formatSymbol(veToken?.symbol)}</Typography>
-                          </div>
-                        </div>
-                      </MenuItem>
-                    )
-                  })}
-                </Select>
-              </div>
-            </Grid>
-          </Grid>
-        </div>
-      </div>
-    )
-  }
+    );
+  };
 
   return (
-    /*<>
-      <Typography className={classes.notiyText}>
-        The page is under<br/>development
-      </Typography>
-    </>*/
-    <div className={ classes.container}>
-      <div className={ classes.searchBar }>
-      <Grid container spacing={2}>
-        <Grid item lg={7} md={7} sm={12} xs={12}>
+    <>
+      <div className={['g-flex', 'g-flex--align-center', 'g-flex--space-between'].join(' ')}>
         <TextField
-          className={classes.searchContainer}
-          variant="outlined"
+          autoFocus
           fullWidth
-          placeholder="0x..."
+          placeholder="Paste the token address you want to whitelist: 0x..."
           value={search}
           onChange={onSearchChanged}
+          InputProps={{
+            style: {
+              background: appTheme === 'dark' ? '#151718' : '#DBE6EC',
+              border: '1px solid',
+              borderColor: appTheme === "dark" ? '#5F7285' : '#86B9D6',
+              borderRadius: 0,
+              maxWidth: 700,
+            },
+            classes: {
+              root: classes.searchInput,
+            },
+            startAdornment: <InputAdornment position="start">
+              <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M10.518 9.69309L13.0165 12.1909L12.191 13.0163L9.69321 10.5179C8.76381 11.263 7.60779 11.6682 6.41663 11.6665C3.51863 11.6665 1.16663 9.3145 1.16663 6.4165C1.16663 3.5185 3.51863 1.1665 6.41663 1.1665C9.31463 1.1665 11.6666 3.5185 11.6666 6.4165C11.6683 7.60767 11.2631 8.76368 10.518 9.69309ZM9.34788 9.26025C10.0882 8.49894 10.5016 7.47842 10.5 6.4165C10.5 4.16017 8.67238 2.33317 6.41663 2.33317C4.16029 2.33317 2.33329 4.16017 2.33329 6.4165C2.33329 8.67225 4.16029 10.4998 6.41663 10.4998C7.47854 10.5015 8.49906 10.0881 9.26038 9.34775L9.34788 9.26025Z"
+                  fill={appTheme === "dark" ? '#4CADE6' : '#0B5E8E'}
+                />
+              </svg>
+            </InputAdornment>,
+          }}
           inputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <Search />
-              </InputAdornment>
-            ),
+            className: classes.searchInputText,
+            style: {
+              padding: '10px',
+              borderRadius: 0,
+              border: 'none',
+              fontWeight: 400,
+              fontSize: '18px',
+              lineHeight: '120%',
+              color: appTheme === 'dark' ? '#C6CDD2' : '#325569',
+            },
           }}
         />
-        </Grid>
-        <Grid item lg={5} md={5} sm={12} xs={12}>
-          { renderMediumInput(nft, nfts) }
-        </Grid>
-      </Grid>
 
+        {TokenSelect({
+          value: nft,
+          options: nfts,
+          symbol: veToken?.symbol,
+          handleChange,
+          placeholder: 'Click to select veNFT'
+        })}
       </div>
-      <div className={ classes.results }>
-        { loading && <CircularProgress />}
-        { token && token.address && renderToken()}
+
+      <div className={classes.results}>
+        {loading && <CircularProgress style={{
+          position: 'absolute',
+          top: 200,
+          left: '50%',
+          color: '#ffffff',
+        }}/>}
+        {token && token.address && renderToken()}
       </div>
-    </div>
-  )
+    </>
+  );
 }
