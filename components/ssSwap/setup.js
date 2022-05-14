837681f0d7a5de65a5a2666aecb6a195cd28929b
Maksim Goncharov <magoncharov@tsum.com>
2022-05-14 23:48:14 +0300

    fix swap button color on theme change

--- components/ssSwap/setup.js
+++ components/ssSwap/setup.js
@@ -544,9 +544,9 @@ function Setup() {
     );
   };
 
-  const [swapIconBgColor, setSwapIconBgColor] = useState(appTheme === 'dark' ? '#24292D' : '#B9DFF5');
-  const [swapIconBorderColor, setSwapIconBorderColor] = useState(appTheme === 'dark' ? '#5F7285' : '#86B9D6');
-  const [swapIconArrowColor, setSwapIconArrowColor] = useState(appTheme === 'dark' ? '#ffffff' : '#ffffff');
+  const [swapIconBgColor, setSwapIconBgColor] = useState(null);
+  const [swapIconBorderColor, setSwapIconBorderColor] = useState(null);
+  const [swapIconArrowColor, setSwapIconArrowColor] = useState(null);
 
   const swapIconHover = () => {
     setSwapIconBgColor(appTheme === 'dark' ? '#2D3741' : '#9BC9E4');
