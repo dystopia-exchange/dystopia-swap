1ab3d04906d6a59871649032869ca8365be9c8b9
sanchitdawarsd <sanchitdawar22@gmail.com>
2022-05-25 18:02:53 +0530

    fix: wallet connect fixed

--- stores/accountStore.js
+++ stores/accountStore.js
@@ -210,6 +210,25 @@ class Store {
     }
     return new Web3(provider);
   };
+  connectWalletConnect = async () => {
+    try {
+      const that = this;
+      const provider = that.getStore('connectorsByName')['WalletConnect'];
+      // const provider = connectorsByName[name];
+      await provider.enable();
+      const web3 = new Web3(provider);
+
+      that.setStore({
+        account: { address: provider.accounts[0] },
+        web3context: { library: { provider: web3 }},
+      });
+      return true;
+
+    } catch(e) {
+      console.log(e);
+      return false;
+    }
+  }
 
   getMulticall = async () => {
     const web3 = await this.getWeb3Provider()
