0719d838b118d482385d47104e7a02df1dcd9f64
belbix <7453635@gmail.com>
2022-07-02 10:52:51 +0300

    fix: move back info button after wrong merge

--- components/header/header.js
+++ components/header/header.js
@@ -481,6 +481,24 @@ function Header(props) {
               );
             }}
           </WalletConnect>
+
+          <div
+              className={[classes.statButton, classes[`statButton--${appTheme}`], 'g-flex', 'g-flex--align-center'].join(' ')}
+              onClick={() => window.open("https://info.dystopia.exchange/home", "_blank")}>
+            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                  style={{marginRight: 5}}
+                  d="M1.3335 8.66667H5.3335V14H1.3335V8.66667ZM6.00016 2H10.0002V14H6.00016V2ZM10.6668 5.33333H14.6668V14H10.6668V5.33333Z"
+                  fill={appTheme === 'dark' ? '#4CADE6' : '#0B5E8E'}/>
+            </svg>
+
+            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                  d="M10.6694 6.276L4.93144 12.014L3.98877 11.0713L9.7261 5.33333H4.66944V4H12.0028V11.3333H10.6694V6.276Z"
+                  fill={appTheme === 'dark' ? '#5688A5' : '#5688A5'}/>
+            </svg>
+          </div>
+
           <ThemeSwitcher />
 
           {transactionQueueLength > 0 && (
