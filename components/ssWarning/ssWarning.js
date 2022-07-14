ef4077816b19908c39d475a708cc71214ebb2b7e
artem-kochetkov-94 <artem-kochetkov-94@mail.ru>
2022-07-14 19:24:52 +0500

    update modal

--- components/ssWarning/ssWarning.js
+++ components/ssWarning/ssWarning.js
@@ -24,71 +24,52 @@ export default function ffWarning({close, title, subTitle, icon, description, bt
         paper: appTheme === "dark" ? classes['paperBody--dark'] : classes['paperBody--light'],
       }}>
       <div className={classes.dialogContainer}>
-        <div className={[classes.warningContainer, classes[`warningContainer--${appTheme}`]].join(' ')}>
-          <div
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-            }}>
-            <img
-              src={appTheme === "dark" ? `/images/${icon ? icon : 'icon-warning'}--dark.svg` : `/images/${icon ? icon : 'icon-warning'}.svg`}
-              className={classes.warningIcon}/>
+        <div className={classes.warningContainer}>
+          <div className={classes.header}>
+            <img src="/images/icon-warning.svg" className={classes.warningIcon} />
 
-            <Typography className={[classes.title1, classes[`title1--${appTheme}`]].join(' ')}>
+            <Typography className={classes.title1}>
               {title ? title : 'Dystopia Disclaimer:'}
             </Typography>
           </div>
 
-          <Typography className={[classes.title2, classes[`title2--${appTheme}`]].join(' ')}>
-            {subTitle ? subTitle : 'Acknowledgement of Terms &amp; Conditions of access'}
+          <Typography className={classes.title2}>
+            {subTitle ? subTitle : 'Acknowledgement of Terms & Conditions of access'}
           </Typography>
 
           <Typography
-            className={[classes.paragraph, classes[`paragraph--${appTheme}`]].join(' ')}
+            className={classes.paragraph}
             align="center">
             {description
               ? description
               : <>
-                <span
-                  className={classes.paragraph1}
-                  style={{
-                    color: appTheme === "dark" ? '#ffffff' : '#0A2C40',
-                  }}>
+                <p className={classes.paragraph1}>
                   Use of the Dystopia.exchange website, services, dapp, or application is subject to the following Terms & Conditions and hereby confirm that by proceeding and interacting with the protocoL I am aware of these and accept them in full:
-                </span>
-                <br/><br/>
-                Dystopia.exchange is a smart contract protocol in alpha stage of launch, and even though multiple
-                security
-                audits have been completed on the smart contracts, I understand the risks associated with using the
-                Dystopia protocol and associated functions.
-                <br/><br/>
-                Any interactions that I have with the associated Dystopia protocol apps, smart contracts or any related
+                </p>
+                <p>Dystopia.exchange is a smart contract protocol in alpha stage of launch, and even though multiple
+                security audits have been completed on the smart contracts, I understand the risks associated with using the
+                Dystopia protocol and associated functions.</p>
+                <p>Any interactions that I have with the associated Dystopia protocol apps, smart contracts or any related
                 functions MAY place my funds at risk, and hereby release the Dystopia protocol and its contributors,
-                team
-                members, and service providers from any and all liability with my use of the above-mentioned functions.
-                <br/><br/>
-                I am lawfully permitted to access this site and use the dystopia.exchange application functions, and I
-                am
-                not in contravention of any laws governing my jurisdiction of residence or citizenship.
+                team members, and service providers from any and all liability with my use of the above-mentioned functions.</p>
+                <p>I am lawfully permitted to access this site and use the dystopia.exchange application functions, and I
+                am not in contravention of any laws governing my jurisdiction of residence or citizenship.</p>
               </>
             }
           </Typography>
 
           <div className={classes.buttonsContainer}>
-            <div
-              className={[classes.primaryButton, classes[`primaryButton--${appTheme}`]].join(' ')}
-              onClick={close}>
-              <Typography className={classes.buttonTextPrimary}>
-                {btnLabel1 ? btnLabel1 : 'I understand the risks involved, proceed to the app'}
+            <div className={classes.primaryButton} onClick={action2 ? action2 : navigateToMedium}>
+              <Typography
+                className={classes.buttonTextPrimary}>
+                {btnLabel2 ? btnLabel2 : 'Learn More'}
               </Typography>
+              <img src="/images/ui/explorer.svg" width="20px" />
             </div>
 
-            <div
-              className={[classes.secondaryButton, classes[`secondaryButton--${appTheme}`]].join(' ')}
-              onClick={action2 ? action2 : navigateToMedium}>
-              <Typography
-                className={[classes.buttonTextSecondary, classes[`buttonTextSecondary--${appTheme}`]].join(' ')}>
-                {btnLabel2 ? btnLabel2 : 'Read more about it on the Medium article'}
+            <div className={classes.secondaryButton} onClick={close}>
+              <Typography className={classes.buttonTextSecondary}>
+                {btnLabel1 ? btnLabel1 : 'I understand the risks involved, proceed to the app'}
               </Typography>
             </div>
           </div>
