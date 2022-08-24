4778895726aa964ea2c5c4eeba504cf0eac9ec9c
Alien Deployer <m@a17.name>
2022-08-24 10:51:58 +0300

    multiswap router update

--- components/MultiSwap/constants.js
+++ components/MultiSwap/constants.js
@@ -1,3 +1,3 @@
-export const multiSwapAddress = '0xB6A771eC805a6e69711d822577D4d0516da51378';
+export const multiSwapAddress = '0x11637b94Dfab4f102c21fDe9E34915Bb5F766A8a';
 export const _SLIPPAGE_PRECISION = 10000;
 export const gasLimit = 3000000;
